18520b63a576785b1348a9199b9fd4e2520f7135
Ngibuini Mwaura <ngibuini.mwaura@andela.com>
2017-08-17 21:47:48 +0100

    HTML templating

--- 02_sql.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-with sqlite3.connect("new.db") as connection:
-	cursor = conn.cursor()
-	cursor.execute(" INSERT INTO population VALUES('New York City',\
-			'NY','8400000') ")
-	cursor.execute(" INSERT INTO population VALUES('San Francisco',\
-			'CA','800000')")
